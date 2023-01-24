2ded06bb6e1e0c7315ae30d1e0db526f0bbd9c99
Imane-Rahoui <Imaanerahoui@gmail.com>
2023-01-24 17:15:52 +0100

    bug fixed

--- ecom-app/src/app/guards/security.guard.ts
+++ ecom-app/src/app/guards/security.guard.ts
@@ -21,6 +21,7 @@ export class AuthGuard extends KeycloakAuthGuard {
         route: ActivatedRouteSnapshot,
         state: RouterStateSnapshot
     ) {
+        console.log("auth : "+this.authenticated)
         // Force the user to log in if currently unauthenticated.
         if (!this.authenticated) {
             await this.keycloak.login({
